14bee24e5e9e7029d06d9be9b90b4a85c5c3f61e
Veronika Popova <freebird130188@gmail.com>
2023-04-25 09:01:39 +0200

    Added changes in the index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
-export default function HomePage() {
+import Link from "next/link";
+import Country from "@/src/components/Country/Country";
+import { countries } from "@/lib/db";
+import Header from "@/src/components/Header/Header";
+import styled from "styled-components";
+
+const Stylemain = styled.main`
+  margin-top: 7rem;
+  background-color: #cbf3f0;
+`;
+
+export default function Countries() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <Stylemain>
+      <Header title="All Countries" />
+      <div className="country">
+        {countries.map((country) => {
+          return (
+            <ul key={countries.id}>
+              <Country
+                nameOfCountry={country.name}
+                codeOfcountry={country.code}
+              />
+              <ol>
+                {country.cities.map((city) => (
+                  <ol key={city}>
+                    <Link href={`/DetailsTrip/`}>
+                      {city}
+                    </Link>
+                  </ol>
+                
+                ))}
+              </ol>
+            </ul>
+          );
+        })}
+      </div>
+    </Stylemain>
   );
 }
