6060643c9f3e2f5654b32e157d5d5dab8466be23
Veronika Popova <freebird130188@gmail.com>
2023-05-13 20:35:28 +0200

    Added some styling

--- src/components/HotelList/index.js
+++ src/components/HotelList/index.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import CreateHotel from "@/src/components/Hotel";
 import { v4 as uuidv4 } from "uuid";
-
 import {
   StyledButtonWithDesable,
   StyledSection,
